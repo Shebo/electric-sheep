0b213094a854f2b4d49c215c5cafb7610132aa07
Shebo <shebo6@gmail.com>
2016-05-22 16:36:09 +0300

    Test Commit

--- archive.php
+++ archive.php
@@ -13,18 +13,7 @@ get_header(); ?>
 		<main id="main" class="site-main" role="main">
 
 		<?php
-		if ( have_posts() ) : ?>
-
-			<header class="page-header">
-				<?php
-					the_archive_title( '<h1 class="page-title">', '</h1>' );
-					the_archive_description( '<div class="taxonomy-description">', '</div>' );
-				?>
-			</header><!-- .page-header -->
-
-			<?php
-			/* Start the Loop */
-			while ( have_posts() ) : the_post();
+		if ( have_posts() ) : while ( have_posts() ) : the_post();
 
 				/*
 				 * Include the Post-Format-specific template for the content.
